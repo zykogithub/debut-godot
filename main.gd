15c9c7c1bf9b9a4d8c907f66396ce139b9bd080a
Zyko <modaherry@gmail.com>
2025-12-25 14:12:04 +0100

    toute fin du jeu

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var mob_scene : PackedScene
-var score : int
+var score : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,28 +19,34 @@ func new_game() -> void :
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	$HUD/MessageTimer.start()
+	get_tree().call_group("mobs","queue_free")
+	$Music.play()
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
 	var mob_location = $MobPath/MobSpawLocation
-	var direction = mob_location.rotation + PI /2
-	var velocity = Vector2(randf_range(150.0,250.0),0.0)
-	
 	mob_location.progress_ratio = randf()
 	mob.position = mob_location.position
+	
+	var direction = mob_location.rotation + PI /2
 	direction += randf_range(-PI /4, PI/4)
 	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0,250.0),0.0)
 	mob.linear_velocity = velocity
 	
 	add_child(mob)
 
 func _on_score_timer_timeout() -> void:
 	score +=1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
